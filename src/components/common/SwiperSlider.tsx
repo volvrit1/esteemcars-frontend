7903b492fa1ed138bfb2bf955d38647edc72d715
Kunal Verma <vkunal591@gmail.com>
2025-03-25 15:15:28 +0530

    updated slider

--- src/components/common/SwiperSlider.tsx
+++ src/components/common/SwiperSlider.tsx
@@ -11,11 +11,12 @@ import Image from "next/image";
 
 const HeroSection = ({ data }: any) => {
   const [loading, setLoading] = useState(true);
-
-  // Handle image loading state
   const handleImageLoad = () => {
     setLoading(false);
   };
+
+  const mobileData = data.filter((item: any) => item.mobile);
+  const bannerData = data.filter((item: any) => !item?.mobile);
 
   // If data is not available, we should set the loading to false immediately
   useEffect(() => {
@@ -33,24 +34,67 @@ const HeroSection = ({ data }: any) => {
         </div>
       )}
 
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        loop={true}
-        autoplay={{
-          delay: 3000,
-        }}
-        navigation={false}
-        pagination={false}
-        modules={[Autoplay, Navigation, Pagination]}
-        className=""
-      >
-        <div className="hidden lg:block">
-          {data &&
-            data
-              .filter((item: any) => item?.mobile !== true) // Filter for mobile items
-              .map((data: any) => (
-                <SwiperSlide key={data?.image}>
+      <div className="hidden md:block">
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          loop={true}
+          autoplay={{
+            delay: 3000,
+          }}
+          navigation={false}
+          pagination={false}
+          modules={[Autoplay, Navigation, Pagination]}
+          className="h-auto lg:h-screen hidden lg:block"
+        >
+          {bannerData &&
+            bannerData.map((data: any) => (
+              <SwiperSlide key={data?.image}>
+                <Image
+                  priority
+                  width={100}
+                  height={100}
+                  unoptimized
+                  alt={data?.title}
+                  src={`${process.env.NEXT_PUBLIC_BASE_URL}${
+                    data?.image || "/assets/banner002.png"
+                  }`}
+                  className="object-cover w-full" // hidden on mobile, shown on larger screens
+                  onLoad={handleImageLoad} // Set the image load handler
+                />
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </div>
+      <div className="md:hidden">
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          loop={true}
+          autoplay={{
+            delay: 3000,
+          }}
+          navigation={false}
+          pagination={false}
+          modules={[Autoplay, Navigation, Pagination]}
+          className="h-auto lg:h-screen lg:hidden"
+        >
+          {mobileData &&
+            mobileData.map((data: any) => (
+              <SwiperSlide key={data?.image}>
+                <picture>
+                  <source
+                    media="(max-width: 639px)"
+                    srcSet={`${process.env.NEXT_PUBLIC_BASE_URL}${
+                      data?.image || "/assets/banner002.png"
+                    }`}
+                  />
+                  <source
+                    media="(min-width: 640px)"
+                    srcSet={`${process.env.NEXT_PUBLIC_BASE_URL}${
+                      data?.image || "/assets/banner002.png"
+                    }`}
+                  />
                   <Image
                     priority
                     width={100}
@@ -60,49 +104,14 @@ const HeroSection = ({ data }: any) => {
                     src={`${process.env.NEXT_PUBLIC_BASE_URL}${
                       data?.image || "/assets/banner002.png"
                     }`}
-                    className="object-contain w-full" // hidden on mobile, shown on larger screens
+                    className="object-cover w-full "
                     onLoad={handleImageLoad} // Set the image load handler
                   />
-                </SwiperSlide>
-              ))}
-        </div>
-
-        <div className="lg:hidden">
-          {data &&
-            data
-              .filter((item: any) => item?.mobile === true) // Filter for mobile items
-              .map((data: any) => (
-                <SwiperSlide key={data?.image}>
-                  <picture>
-                    <source
-                      media="(max-width: 639px)"
-                      srcSet={`${process.env.NEXT_PUBLIC_BASE_URL}${
-                        data?.image || "/assets/banner002.png"
-                      }`}
-                    />
-                    <source
-                      media="(min-width: 640px)"
-                      srcSet={`${process.env.NEXT_PUBLIC_BASE_URL}${
-                        data?.image || "/assets/banner002.png"
-                      }`}
-                    />
-                    <Image
-                      priority
-                      width={100}
-                      height={100}
-                      unoptimized
-                      alt={data?.title}
-                      src={`${process.env.NEXT_PUBLIC_BASE_URL}${
-                        data?.image || "/assets/banner002.png"
-                      }`}
-                      className="object-contain w-full "
-                      onLoad={handleImageLoad} // Set the image load handler
-                    />
-                  </picture>
-                </SwiperSlide>
-              ))}
-        </div>
-      </Swiper>
+                </picture>
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </div>
     </section>
   );
 };
