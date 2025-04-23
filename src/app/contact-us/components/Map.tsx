14b1ddc46ce4c5682a7510c64f3c7f9f37409b28
Kunal Verma <vkunal591@gmail.com>
2025-04-23 11:54:56 +0530

    added adress map

--- src/app/contact-us/components/Map.tsx
+++ src/app/contact-us/components/Map.tsx
@@ -4,10 +4,10 @@ export default function Map() {
   return (
     <div className="relative w-full h-[450px]">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12763.271644964827!2d174.67997127552943!3d-36.89470411218473!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d46cf3105d35f%3A0x78390d6b31f5df1b!2s42%20Rosebank%20Road%2C%20Avondale%2C%20Auckland%201026%2C%20New%20Zealand!5e0!3m2!1sen!2sin!4v1742550462728!5m2!1sen!2sin"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3190.5005899008834!2d174.68015737481164!3d-36.9022938824968!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d4149a65ff2d1%3A0x4767465e96c27a59!2s13%20Binsted%20Road%2C%20New%20Lynn%2C%20Auckland%200600%2C%20New%20Zealand!5e0!3m2!1sen!2sin!4v1745389374989!5m2!1sen!2sin"
         width="600"
         height="450"
-        style={{ border: 0 }}
+        style={{border:"0"}}
         allowFullScreen
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
